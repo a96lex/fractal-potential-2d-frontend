3e7b5775db4339cc9d418beef489fe702d40081f
a96lex <a96lex@gmail.com>
2022-11-08 17:48:40 +0100

    update style

--- src/components/Results.js
+++ src/components/Results.js
@@ -37,79 +37,71 @@ export default function Results() {
   }, [excitedState, recursion]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        marginTop: 10,
-        height: "100%",
-        alignItems: "center",
-      }}
-    >
-      <div
-        className="title"
-        style={{ alignSelf: "flex-start", marginLeft: "10%" }}
-      >
-        Results
-      </div>
+    <div className="section-container">
       <div
         style={{
           display: "flex",
-          flexDirection: "row",
-          width: "80%",
-          justifyContent: "space-around",
-          alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <div className="parameter-container">
-          <div className="nav-link">Invert the fractal</div>
-          <input
-            type="checkbox"
-            className="custom-input"
-            value={reversed}
-            onChange={() => setReversed(!reversed)}
-          />
-        </div>
-        <div className="parameter-container">
-          <div className="nav-link">Recursion value</div>
-          <input
-            className="custom-input"
-            type="number"
-            min="1"
-            max="4"
-            value={recursion}
-            onChange={(e) => setRecursion(e.target.value)}
-          />
-        </div>
-        <div className="parameter-container">
-          <div className="nav-link">Excited state</div>
-          <input
-            type="number"
-            className="custom-input"
-            min="0"
-            max={maxState}
-            value={excitedState}
-            onChange={(e) => setExcitedState(e.target.value)}
-          />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <div className="title">Results</div>
+          <div className="parameter-container">
+            <div className="nav-link">Invert the fractal</div>
+            <input
+              type="checkbox"
+              className="custom-input"
+              value={reversed}
+              onChange={() => setReversed(!reversed)}
+            />
+          </div>
+          <div className="parameter-container">
+            <div className="nav-link">Recursion value</div>
+            <input
+              className="custom-input"
+              type="number"
+              min="1"
+              max="4"
+              value={recursion}
+              onChange={(e) => setRecursion(e.target.value)}
+            />
+          </div>
+          <div className="parameter-container">
+            <div className="nav-link">Excited state</div>
+            <input
+              type="number"
+              className="custom-input"
+              min="0"
+              max={maxState}
+              value={excitedState}
+              onChange={(e) => setExcitedState(e.target.value)}
+            />
+          </div>
         </div>
       </div>
       <div
         style={{
           display: "flex",
           flexDirection: "row",
-          width: "80%",
+          width: "100%",
           justifyContent: "space-around",
           alignItems: "center",
         }}
       >
         <img
-          style={{ height: 398, boxShadow: "0 0 1px 1px rgba(0,0,0,1)" }}
+          className="fractal-img"
           src={fractalUrl}
           alt="image not found"
         ></img>
         <img
-          style={{ height: 398, boxShadow: "0 0 1px 1px rgba(0,0,0,1)" }}
+          className="fractal-img"
           src={excitedUrl}
           alt="image not found"
         ></img>
